132adbfdc774306df7c96476291bccc803c31b4d
Martin Nordlund <nordlund.martin@gmail.com>
2020-12-17 14:06:33 +0100

    sf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import Rank from './Components/Rank/Rank';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Clarifai from 'clarifai';
 
-function App() {
+
+const app = new Clarifai.App({
+   apiKey: '5bcc2debae5c47e0a438ea55b614d51b'
+  });
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+
+
+
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+     app.models
+      .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+      .then(response => {
+        console.log('hi', response)
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+    }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation/>
+      <Logo/>
+      <Rank/>
+      <ImageLinkForm
+        onInputChange={this.onInputChange}
+        onButtonSubmit= {this.onButtonSubmit}
+      />
+
     </div>
   );
+}
 }
 
 export default App;
